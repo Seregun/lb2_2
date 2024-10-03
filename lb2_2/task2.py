773cd32b0c1fe7bf52731520f5d1cc37434e2ec7
Seregun <seregunmgp@gmail.com>
2024-10-03 09:39:25 +0300

    Delete lb2_2 directory

--- lb2_2/task2.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Введите кол-во школьников: "))
-k = int(input("Введите кол-во яблок: "))
-apples_to_pupils = k//n
-apples_left = k%n12
-print("Кол-во яблок для каждого школьника: ", apples_to_pupils)
-print("Кол-во яблок осталось: ", apples_left)
